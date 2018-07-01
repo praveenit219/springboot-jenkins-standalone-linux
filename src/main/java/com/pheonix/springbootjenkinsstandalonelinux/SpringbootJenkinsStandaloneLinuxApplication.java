0a97279861c6ad546b21cc7ad1efb633ee2ed098
praveenit219 <praveen.tirunamali@gmail.com>
2018-07-01 16:53:34 +0800

    initialized files

---
+++ src/main/java/com/pheonix/springbootjenkinsstandalonelinux/SpringbootJenkinsStandaloneLinuxApplication.java
@@ -0,0 +1,12 @@
+package com.pheonix.springbootjenkinsstandalonelinux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsStandaloneLinuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsStandaloneLinuxApplication.class, args);
+	}
+}
